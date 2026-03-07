676f4adec85dc5b14017faa2f3c6e8194358399b
Luis <instagram.neutron083@slmails.com>
2026-03-07 11:20:40 +0000

    Update num_elem.pl

--- num_elem.pl
+++ num_elem.pl
@@ -5,7 +5,14 @@
    número de elementos de Lista.
    
 Principio de inducción
-1. num_elem([], ).
+1. num_elem([],0).
 2. num_elem(n-1) -> num_elem(n)
 
 */
+
+
+num_elem([],0).
+
+num_elem([_|Resto],N2)  :- num_elem(Resto,N), N2 is N+1.
+
+
