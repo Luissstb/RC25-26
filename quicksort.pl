e6d85dd01622fe00239ccae270b17cc45112aa7b
Luis <instagram.neutron083@slmails.com>
2026-03-19 09:20:58 +0100

    Update quicksort.pl

--- quicksort.pl
+++ quicksort.pl
@@ -17,3 +17,8 @@ divide(_,[],[],[]).
 divide(Elem,[Cab|Resto], [Cab|Menores] , Mayores ) :- Cab =< Elem, divide(Elem,Resto,Menores,Mayores).
 
 divide(Elem,[Cab|Resto], Menores , [Cab|Mayores]) :- Cab > Elem, divide(Elem,Resto,Menores,Mayores).
+
+
+quicksort([], []).
+
+quicksort([Cab|Resto], R3) :- divide(Cab,Resto,Menores,Mayores), quicksort(Menores, R1), quicksort(Mayores,R2), append(R1,[Cab|R2],R3). 
