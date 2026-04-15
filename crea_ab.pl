ca424fa97eef4a12497ffa5f627042a77185444b
Luis <instagram.neutron083@slmails.com>
2026-04-15 11:26:11 +0200

    Update crea_ab.pl

--- crea_ab.pl
+++ crea_ab.pl
@@ -4,14 +4,18 @@
  
  variante:
  
- crea_ab(+N, -ArbolBinario) es cierto cuando ArbolBinario unifica con un arbol binario BALANCEADO que contiene todas las etiquetas sin repeticion desde 1 a N
+ crea_abb(+N, -ArbolBinario) es cierto cuando ArbolBinario unifica con un arbol binario BALANCEADO que contiene todas las etiquetas sin repeticion desde 1 a N
 
 */
+
 
 arbol1(a(1, a(2,nil,nil), a(3,nil, a(4,nil,nil)) )).
 
 crea_ab(ListaEtiquetas, ArbolBinario) : is_list(ListaEtiquetas), !, construir_arbol_balanceado(ListaEtiquetas, ArbolBinario).
 
 crea_ab(N, ArbolBinario) :- integer(N), N >= 0, !, (   N =:= 0->  Lista = [] ;   findall(X, between(1, N, X), Lista) ), construir_arbol_balanceado(Lista, ArbolBinario).
+
+
+crea_abb(
 
 
