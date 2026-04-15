e24c691c702d6c25ec35a623436bfb577e10010d
Luis <instagram.neutron083@slmails.com>
2026-04-15 10:37:07 +0200

    Update crea_ab.pl

--- crea_ab.pl
+++ crea_ab.pl
@@ -6,9 +6,6 @@
  
  crea_ab(+N, -ArbolBinario) es cierto cuando ArbolBinario unifica con un arbol binario BALANCEADO que contiene todas las etiquetas sin repeticion desde 1 a N
 
-
- construir_arbol_balanceado(+ArbolBinario) es cierto si ArbolBinario unifica con un arbol binario que para todo nodo la diferencia entre la altura del arbol izquierdo y derecho es como maximo 1
-
 */
 
 arbol1(a(1, a(2,nil,nil), a(3,nil, a(4,nil,nil)) )).
@@ -17,8 +14,4 @@ crea_ab(ListaEtiquetas, ArbolBinario) : is_list(ListaEtiquetas), !, construir_ar
 
 crea_ab(N, ArbolBinario) :- integer(N), N >= 0, !, (   N =:= 0->  Lista = [] ;   findall(X, between(1, N, X), Lista) ), construir_arbol_balanceado(Lista, ArbolBinario).
 
-construir_arbol_balanceado([], nil).
 
-construir_arbol_balanceado(Lista, arbol(Izq, Raiz, Der)) :- Lista = [_|_], length(Lista, Longitud), Mitad is Longitud // 2, length(ListaIzq, Mitad), append(ListaIzq, [Raiz | ListaDer], Lista),
-    construir_arbol_balanceado(ListaIzq, Izq),
-    construir_arbol_balanceado(ListaDer, Der).
