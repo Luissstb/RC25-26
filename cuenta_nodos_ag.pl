d6276f63e6334189f8591b6f979e31f46e3120fa
Luis <instagram.neutron083@slmails.com>
2026-04-22 11:31:12 +0200

    Add files via upload

---
+++ cuenta_nodos_ag.pl
@@ -0,0 +1,24 @@
+
+
+
+arbolg1( a(a, [a(b,[a(e,[])]),a(c,[]),a(d,[])])  ).
+
+
+/*
+ 
+  cuenta_nodos(+ArbolGen, -Num) es cierto si Num unifica con el numero de nodos de ArbolGen.
+  
+  cuenta_nodos_lista_arboles( +ListaArboles, -Num) es cierto si Num unifica con el numero de nodos de la lista de arboles genericos ListaArboles.
+
+ 
+  Aqui no hay recursividad porque hay 2 predicados diferentes:
+*/
+
+cuenta_nodos( a(_,ListaArboles), R2 ) :- cuenta_nodos(ListaArboles,R), R2 is R + 1.
+
+
+
+cuenta_nodos_lista_arboles([],0).
+
+cuenta_nodos_lista_arboles( [Cab|Resto], Total ) :- cuenta_nodos_lista_arboles(Resto,R), cuenta_nodos(Cab,RCa), Total is R + RCa.
+
